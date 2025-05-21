166c9636e198cfbe156126d35c0fbca27bea10b9
AB <35473718+arsalanbardsiri@users.noreply.github.com>
2025-05-21 13:23:29 -0700

    Add Image and Link, now desire to switch to server side render

--- fe/src/app/page.tsx
+++ fe/src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
 
   useEffect(() => {async function rawData(){
     try{
-      const res = await fetch(`${process.env.NEXT_PUBLIC_STRAPI_URL}/projects?populate=*`)
+      const res = await fetch(`${process.env.STRAPI_URL}/api/projects?populate=*`)
       if (!res.ok) throw new Error(`Failed to fetch, status ${res.status}`);
       const json = await res.json()
       setProjects(json.data)
@@ -29,19 +29,21 @@ export default function Home() {
 },[]);
   
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div>
     <header>
     <p>Header</p>
     </header>
-    <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-    <h1>Hello World</h1>
+    <main >
+    <h1>Projects</h1>
     <ul>
-    <li key = {projects.id}>{projects.map((project)=>(
-      project.title))}
-    </li>
+    {projects.map((project: Project[])=>
+      <li key={project.id}>{project.title}
+      <Image src={`${process.env.NEXT_PUBLIC_STRAPI_URL}${project?.screenshot?.url}`} width={150} height={150} alt={project.title} unoptimized/>
+      <Link className="bg-sky-500" href={project.github_url}  target="_blank" rel="noopener noreferrer">Link</Link>
+      </li>)}
     </ul>
     </main>
-    <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+    <footer>
     <p>Footer</p>
     </footer>
     </div>
